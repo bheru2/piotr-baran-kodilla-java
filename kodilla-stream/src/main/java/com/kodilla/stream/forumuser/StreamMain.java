ff52440712b59b30b55f873c61b9ac96f7e8f799
Bheru <270310@gmail.com>
2019-05-18 18:21:04 +0100

    Task7.3 - rename class user

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/StreamMain.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream.forumuser;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        Forum forum = new Forum(20);
+        Map<Integer, ForumUser> result = forum.getForumUsers().stream()
+                .filter(user -> user.getSex() == 'M')
+                .filter(user -> user.getDateOfBorn().getYear() > 1999)
+                .filter(user -> user.getPostNumber() > 40)
+                .collect(Collectors.toMap(ForumUser::getUserID, user -> user));
+
+        result.entrySet().stream().map(entry -> entry.getKey() + " : " + entry.getValue()).forEach(System.out::println);
+    }
+}
