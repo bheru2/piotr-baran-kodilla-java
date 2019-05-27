4472cf9a92c82ea2ae0b5623d4157517ff2cdee1
Bheru <270310@gmail.com>
2019-05-27 21:44:16 +0100

    Squashed commit of the following:
    commit 57de2a59312d8d487e147ba0020cbb892d6e7005
    Merge: 98a3bd6 1d8f48a
    Author: bheru2 <49920811+bheru2@users.noreply.github.com>
    Date:   Mon May 27 21:33:53 2019 +0100
    
        Merge pull request #12 from bheru2/Task7.6
    
        Task7.6
    
    commit 98a3bd6218453e98c3e6dd46eb8272ecf03a6cb1
    Merge: e028d35 d584208
    Author: bheru2 <49920811+bheru2@users.noreply.github.com>
    Date:   Mon May 27 21:33:39 2019 +0100
    
        Merge pull request #14 from bheru2/Task8.3
    
        Task8.3
    
    commit e028d35c2814657ff1545aab0d6c388a4c34cf48
    Merge: 1859906 3fbe07b
    Author: bheru2 <49920811+bheru2@users.noreply.github.com>
    Date:   Mon May 27 21:33:20 2019 +0100
    
        Merge pull request #8 from bheru2/Task7.3
    
        Task7.3
    
    commit 1859906e0406e9b7b2a14968b223390ddce02861
    Merge: 39766c3 53f32f4
    Author: bheru2 <49920811+bheru2@users.noreply.github.com>
    Date:   Mon May 27 21:32:50 2019 +0100
    
        Merge pull request #13 from bheru2/Task8.2
    
        Task 8.2
    
    commit d5842080b20a1796de363bc1657ada58c7dfe2b2
    Author: Bheru <270310@gmail.com>
    Date:   Mon May 27 18:58:42 2019 +0100
    
        Task 8.3
    
    commit 53f32f4fb96625779e897f669fafcb587359b909
    Author: Bheru <270310@gmail.com>
    Date:   Mon May 27 18:50:52 2019 +0100
    
        Task 8.2
    
    commit 3fbe07b3d44047e91b80f4ffd2e7fcffda256c20
    Author: Bheru <270310@gmail.com>
    Date:   Mon May 27 18:33:25 2019 +0100
    
        Task 7.6
    
    commit 1d8f48a98f5484e5e5e986d4b0ccd43d120b7c46
    Author: Bheru <270310@gmail.com>
    Date:   Mon May 27 18:16:16 2019 +0100
    
        Task 7.6
    
    commit 1d8ec18a017271f8d882e700e47bb353b5be3f2e
    Author: Bheru <270310@gmail.com>
    Date:   Sun May 19 17:46:34 2019 +0100
    
        Task 7.6
    
    commit 7dd7134d1114f951089a5cbecdc6e420b98eb96c
    Author: Bheru <270310@gmail.com>
    Date:   Sun May 19 14:19:22 2019 +0100
    
        Task 7.6
    
    commit ff52440712b59b30b55f873c61b9ac96f7e8f799
    Author: Bheru <270310@gmail.com>
    Date:   Sat May 18 18:21:04 2019 +0100
    
        Task7.3 - rename class user
    
    commit d8cb819b44671233b2b60565c5db022a2c1aaa01
    Author: Bheru <270310@gmail.com>
    Date:   Fri May 17 23:44:52 2019 +0100
    
        Task7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/StreamMain.java
@@ -1,20 +1,29 @@
 package com.kodilla.stream.beautifier;
 
-import com.kodilla.stream.forumuser.ForumUser;
-import com.kodilla.stream.forumuser.User;
-
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class StreamMain {
     public static void main(String[] args) {
-        User users = new User();
-        Map<Integer, ForumUser> result = users.getForumUsers().stream()
-                .filter(user -> user.getSex() == 'M')
-                .filter(user -> user.getDateOfBorn().getYear() > 1999)
-                .filter(user -> user.getPostNumber() > 40)
-                .collect(Collectors.toMap(ForumUser::getUserID, user -> user));
-
-        result.entrySet().stream().map(entry -> entry.getKey() + " : " + entry.getValue()).forEach(System.out::println);
+        String sampleText = "Test Text";
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println(poemBeautifier.beautify(sampleText, decorate -> sampleText.toLowerCase()));
+        System.out.println(poemBeautifier.beautify(sampleText, decorate -> sampleText.toUpperCase()));
+        System.out.println(poemBeautifier.beautify(sampleText, decorate -> {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < sampleText.length(); i++) {
+                if (i % 2 == 0) {
+                    sb.append(sampleText.substring(i, i + 1).toUpperCase());
+                } else {
+                    sb.append(sampleText.substring(i, i + 1).toLowerCase());
+                }
+            }
+            return sb.toString();
+        }));
+        System.out.println(poemBeautifier.beautify(sampleText, decorate -> {
+            char[] reverse = sampleText.toCharArray();    // instead of StringBuilder.reverse
+            StringBuilder sb = new StringBuilder();
+            for (int i = reverse.length - 1; i >= 0; i--) {
+                sb.append(reverse[i]);
+            }
+            return sb.toString();
+        }));
     }
 }
